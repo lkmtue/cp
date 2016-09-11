5f489f703750cfdff962da90bbe158d3f072c5a6
Tue Le <lkmth1@gmail.com>
2016-09-12 00:55:59 +0800

    Snapshot 12 Sep

---
+++ cf/bubble-cup-final/t.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORN(i, a, b) for (int i = (a); i < (b); i++)
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); i--)
+#define BUG(x) cerr << #x << " = " << x << endl
+
+int main() {
+}
+
